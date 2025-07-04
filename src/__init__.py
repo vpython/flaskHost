80f7357eb66dbc95f31d135d99bd85284e246f8f
Steve Spicklemire <steve@spvi.com>
2025-07-04 10:07:29 -0400

    Fix spelling in comment.

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,8 @@
 #
-# import some things so they are availabel to main.py
+# import some things so they are available to main.py
 #
 
+from . import routes
 from flask import Flask
 
 app = Flask(__name__, static_folder='../static', static_url_path='/')
-
-from . import routes
