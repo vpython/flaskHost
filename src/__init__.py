f22850ac5b8cc5f6fdd4e5c67e20f9bcc54f4c1a
Steve Spicklemire <steve@spvi.com>
2025-07-20 07:33:33 -0400

    circular route issue.

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,8 @@
 # import some things so they are available to main.py
 #
 
-from . import routes
 from flask import Flask
 
 app = Flask(__name__, static_folder='../static', static_url_path='/')
+
+from . import routes
