698e64012042585f0ee425158add31b15a12f75a
Steve Spicklemire <steve@spvi.com>
2026-06-13 15:35:11 -0400

    fix: add COOP/COEP headers for SharedArrayBuffer support in wmWVPRunner
    - Add after_request handler to set Cross-Origin-Opener-Policy and Cross-Origin-Embedder-Policy headers
    - Required for Web Worker synchronization in wmWVPRunner (SharedArrayBuffer/Atomics.wait)
    - Allows wmWVPRunner iframe to access SharedArrayBuffer for synchronous rate() calls

--- src/__init__.py
+++ src/__init__.py
@@ -6,4 +6,11 @@ from flask import Flask
 
 app = Flask(__name__, static_folder='../static', static_url_path='/')
 
+# Add COOP/COEP headers for SharedArrayBuffer support in Web Workers (wmWVPRunner)
+@app.after_request
+def add_coop_coep_headers(response):
+	response.headers['Cross-Origin-Opener-Policy'] = 'same-origin'
+	response.headers['Cross-Origin-Embedder-Policy'] = 'require-corp'
+	return response
+
 from . import routes
